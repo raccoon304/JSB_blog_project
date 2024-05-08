aea0a0076ed76c8de10bb76b7e8e483ac76ae28b
young <smon0376@naver.com>
2024-05-08 16:12:16 +0900

    05_08_blog_javaspringboot(login_logout)

---
+++ src/main/java/me/young/springbootdeveloper/service/UserService.java
@@ -0,0 +1,22 @@
+package me.young.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.young.springbootdeveloper.domain.User;
+import me.young.springbootdeveloper.dto.AddUserRequest;
+import me.young.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
